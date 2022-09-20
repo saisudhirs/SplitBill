1b344afe6486f34ecc2580cd0fa2827fbf84ab78
Sai Sudhir <saisudhirs@outlook.com>
2022-09-20 11:18:33 +0530

    BUG: repeated names accepted fixed, non initialized usage fixed

--- asymmetric_v2.py
+++ asymmetric_v2.py
@@ -22,46 +22,92 @@ Money transfers by UPI from app(if RBI approves :-P
 '''
 from typing import Dict, Any
 import math
+from datetime import datetime
 
 master: dict[Any, Any] = {}
 balSheet: dict[Any, Any] = {}
 instSheet: dict[Any, Any] = {}
 expSheet: dict[Any, Any] = {"All expenses": {}}
+expSheet["All expenses"]["GRAND TOTAL"] = 0
+allStatements = []
+
+
+def sanitised_input(prompt, type_=None, min_=None, max_=None, range_=None):
+    if min_ is not None and max_ is not None and max_ < min_:
+        raise ValueError("min_ must be less than or equal to max_.")
+    while True:
+        ui = input(prompt)
+        if type_ is not None:
+            try:
+                ui = type_(ui)
+            except ValueError:
+                print("Input type must be {0}.".format(type_.__name__))
+                continue
+        if max_ is not None and ui > max_:
+            print("Input must be less than or equal to {0}.".format(max_))
+        elif min_ is not None and ui < min_:
+            print("Input must be greater than or equal to {0}.".format(min_))
+        elif range_ is not None and ui not in range_:
+            if isinstance(range_, range):
+                template = "Input must be between {0.start} and {0.stop}."
+                print(template.format(range_))
+            else:
+                template = "Input must be {0}."
+                if len(range_) == 1:
+                    print(template.format(*range_))
+                else:
+                    expected = " or ".join((
+                        ", ".join(str(x) for x in range_[:-1]),
+                        str(range_[-1])
+                    ))
+                    print(template.format(expected))
+        else:
+            return ui
 
 
 def intro():
-    name = input("Enter member name: ")
-    memExp: dict[Any, Any] = {}
-    # master[name] = 0
-    instSheet[name] = 0
-    expSheet[name] = memExp
-    expSheet[name]["TOTAL"] = 0
+    condition = 1
+    while condition:
+        name = sanitised_input("Enter member name: ", str)
+        if name not in instSheet:
+            memExp: dict[Any, Any] = {}
+            # master[name] = 0
+            instSheet[name] = 0
+            expSheet[name] = memExp
+            expSheet[name]["TOTAL"] = 0
+            condition = 0
+        else:
+            print("Name already exists.")
 
 
 def intro0():
-    Member_Q = int(input("How many members? "))
+    Member_Q = sanitised_input("How many members? ", int, 1)
     for i in range(0, Member_Q):
         intro()
 
 
 def instNonUnif():
-    expense = input("Name of expense: ")
-    print(instSheet.keys())
-    payee = input("Who is paying? ")
-    subs = input("Subscribers (seperated by just comma): (* for all) ")
+    expense = sanitised_input("Name of expense: ", str)
+    members = (instSheet.keys())
+    print(members)
+    payee = sanitised_input("Who is paying? ", str, range_= members)
+    subs = input("Subscribers (seperated by just comma): (* for all) ") #has to be integrated with santised_input
     if subs == '*':
         subsList = list(instSheet.keys())
     else:
         subsList = subs.split(",")
-    payeeVal = float(input("Payment value: "))
+    payeeVal = sanitised_input("Payment value: ",float, 0)
     # master[payee] -= payeeVal
     instVal = payeeVal / len(subsList)
     instVal = round(instVal, 5)
-    for i in instSheet:
-        if i in subsList:
-            instSheet[i] += instVal
-            expSheet[i][expense] = instVal
-            expSheet[i]["TOTAL"] += instVal  # totalling should be optimised
+    while len(subsList) != 0:
+        for i in instSheet:
+            if i in subsList:
+                instSheet[i] += instVal
+                expSheet[i][expense] = instVal
+                expSheet[i]["TOTAL"] += instVal  # totalling should be optimised
+                print(subsList)
+                subsList.pop(0)
     expSheet["All expenses"]["GRAND TOTAL"] += payeeVal
     expSheet["All expenses"][expense] = payeeVal
     instSheet[payee] -= payeeVal
@@ -85,14 +131,16 @@ def transaction():
             b = min(instSheet, key=instSheet.get)
             av = round(instSheet[a], est)
             bv = round(instSheet[b], est)
-            print("\n***", a, "gives", abs(av), "to", b, "***\n")
+            statement = ("\n***     " + str(a) + " gives " + str(abs(av)) + " to " + str(b) + "     ***\n")
         else:
             b = max(instSheet, key=instSheet.get)
             a = min(instSheet, key=instSheet.get)
             av = round(instSheet[a], est)
             bv = round(instSheet[b], est)
-            print("\n***", b, "gives", abs(av), "to", a, "***\n")
+            statement = ("\n***     " + str(b) + " gives " + str(abs(av)) + " to " + str(a) + "     ***\n")
 
+        print(statement)
+        allStatements.append(statement)
         instSheet[b] = bv + av
         instSheet[a] = 0
     print(instSheet)
@@ -101,35 +149,64 @@ def transaction():
     print("\n***\n")
 
 
-def main():  # not sure if this is still true: loop is working only for order 0 -> 1 -> 2
-    op = (
-        input(
-            "Options:\n0 for initializing members\n1 for new expense\n2 for showing balance transactions\n-----\nInput: "))
+def saveTrans():
+    cond = 1
+    while cond == 1:
+        save_stat = "Do you want to save the transactions? (y/n): "
+        op = input(save_stat).lower()
+        if  op == "y":
+
+            file = (open("balance.txt", "w"))
+
+            now = datetime.now()
+
+            dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+            print("date and time =", dt_string)
+            file.write("At ")
+            file.write(dt_string)
+
+            for statement in allStatements:
+                file.write(statement)
+
+
+            #file.write((key, ':', value) for key, value in expSheet.items())
+
+            file.close()
+            cond = 0
+
+        elif op == "n":
+            cond = 0
+
+        else:
+            print("Wrong Input")
+            op = input(op)
+
+
+def main():  ## not sure if this is still true: loop is working only for order 0 -> 1 -> 2
+    init_stat = "Options:\n0 for initializing members\n1 for new expense\n2 for showing balance transactions\n-----\nInput: "
+
+    op = input(init_stat)
     while 1:
         if op == '0':  # switch case can be used
             intro0()
-            op = (
-                input(
-                    "\n-----\nOptions:\n0 for initializing members\n1 for new expense\n2 for showing balance transactions\n-----\nInput: "))
+            op = input(init_stat)
 
         elif op == '1':
+            if len(instSheet)==0:
+                print("Members not initialized.")
+                intro0()
             instNonUnif()
-            op = (
-                input(
-                    "\n-----\nOptions:\n0 for initializing members\n1 for new expense\n2 for showing balance transactions\n-----\nInput: "))
+            op = input(init_stat)
 
         elif op == '2':
             # balance()
             transaction()
-            op = (
-                input(
-                    "\n-----\nOptions:\n0 for initializing members\n1 for new expense\n2 for showing balance transactions\n-----\nInput: "))
+            saveTrans()
+            op = input(init_stat)
 
         else:
             print("Wrong Input")
-            op = (
-                input(
-                    "\n-----\nOptions:\n0 for initializing members\n1 for new expense\n2 for showing balance transactions\n-----\nInput: "))
+            op = input(init_stat)
 
 
 main()
